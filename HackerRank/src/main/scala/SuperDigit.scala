62a8f14e1a5d832e205805b5dbf8196dd5080a22
Brian Vegh <brianv.revature@gmail.com>
2022-03-09 16:16:22 -0500

    Added InputValidation Repo, contains InputValid.scala CLI input validation class

---
+++ HackerRank/src/main/scala/SuperDigit.scala
@@ -0,0 +1,21 @@
+//package hackerRank
+//
+//import scala.util.Try
+//
+//object SuperDigit {
+//
+//  def p(n:Int,k:Int):Int={
+//    val
+//    for (_ <-0 until(k)) {
+//      n_s.concat(n.toString)
+//    }
+//
+//  }
+//
+//
+//  def main(args:Array[String]):Unit={
+//
+//
+//
+//  }
+//}
